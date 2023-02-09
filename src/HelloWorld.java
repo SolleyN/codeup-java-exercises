e10a549d5969bdce9af2fc2fda32fb099ca22726
Nickolas Solley <nicksolley@gmail.com>
2023-02-09 13:44:08 -0600

    Console IO Lecture

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,32 +8,32 @@ public class HelloWorld {
 //        String myString= 'c';
         System.out.println(myString);
 //        myString ="3.14159"
-        long = myNumber;
-        System.out.println(myNumber);
-        myNumber = 3.14;
-        myNumber = 123L;
-        myNumber = 123;
-        myNumber = 3.14F;
-        int x = 5;
-        System.out.println(x++);
-        System.out.println(x);
-        int x = 5;
-        System.out.println(++x);
-        System.out.println(x);
-
-        int x = 4;
-        x = x + 5;
-
-        int x = 4 + 5;
-
-        int x = 3;
-        int y = 4;
-        y = y * x;
-
-        int x = 10;
-        int y = 2;
-        x = x / y;
-        y = y - x;
+//        long = myNumber;
+//        System.out.println(myNumber);
+//        myNumber = 3.14;
+//        myNumber = 123L;
+//        myNumber = 123;
+//        myNumber = 3.14F;
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//
+//        int x = 4;
+//        x = x + 5;
+//
+//        int x = 4 + 5;
+//
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
     }
 
 
