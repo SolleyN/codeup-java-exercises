92540416cf03c573eb6970a07720d78ec7340100
Nickolas Solley <nicksolley@gmail.com>
2023-02-11 15:49:13 -0600

    Working on MethodsExercises

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -5,17 +5,28 @@ public class MethodsExercises {
     public static void main(String[] args) {
 
         int result1 = addNum(7, 7);
-        System.out.println(result1);
+        System.out.println("Adding numbers: " + "\n" + result1);
 
-        int result2 = multiplyNum(69, 69);
+        int result69 = multiplyNum(69, 69);
 
-        System.out.println(result2);
+        System.out.println("Multiplying Numbers: " + "\n" + result69);
+
+        int result2 = 1;
+        int i;
+        for (i = 1; i <= 10; i++) ;
+        result2 = multiplyNum(result2, i);
+        System.out.println("Multiplying using a for loop: " + "\n"  + result2);
 
         int result3 = divideNum(420, 69);
-        System.out.println(result3);
+        System.out.println("Dividing Numbers: "+ "\n" + result3);
 
         int result4 = subtractNum(240, 96);
-        System.out.println(result4);
+        System.out.println("Subtracting Numbers: "+ "\n" + result4);
+
+        int result5 = modulus(420, 69);
+        System.out.println("The modulus is: " + "\n" + result5);
+
+
     }
 
     // methods need to be placed outside the PSVM method to function properly.
@@ -23,11 +34,11 @@ public class MethodsExercises {
         return x + y;
     }
 
-    public static int multiplyNum(int a, int b) {
-        return a * b;
-    }
+     public static int multiplyNum(int a, int b) {
+       return a * b;
+  }
 
-    public static int divideNum(int a, int b) {
+  public static int divideNum(int a, int b) {
         return a / b;
 
 
@@ -37,6 +48,11 @@ public class MethodsExercises {
         return x - y;
     }
 
+    public static int modulus(int x, int y) {
+
+        return x % y;
+
+    }
 }
 
 
