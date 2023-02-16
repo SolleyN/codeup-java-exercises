077b2e39c00d7307b2b7bff993346370e22a44e1
Nickolas Solley <nicksolley@gmail.com>
2023-02-16 10:17:34 -0600

    array Exercise

--- src/arrayExercise.java
+++ src/arrayExercise.java
@@ -4,6 +4,7 @@
 public class arrayExercise {
 
     public static void main(String[] args) {
+
 //        double numbers[] = {1.0, 1.3, 1.5, 1.7, 1.8};
 //
 //        double sum = 0.0;
@@ -13,15 +14,18 @@ public class arrayExercise {
 //        System.out.println("Your sum is: " + sum);
 
         double[] numbers = new double[5];
-       numbers[0] = 1.5;
-       numbers[1] = 1.7;
-       numbers[2] = 1.3;
-       numbers[3] = 1.2;
-       numbers[4] = 1.6;
-       double sum =0.0;
-       for (double number : numbers) {
-           sum += number;
-       }
+
+        numbers[0] = 1.5;
+        numbers[1] = 1.7;
+        numbers[2] = 1.3;
+        numbers[3] = 1.2;
+        numbers[4] = 1.6;
+
+        double sum = 0.0;
+
+        for (double number : numbers) {
+            sum += number;
+        }
         System.out.println("Your sum is :" + sum);
     }
 
