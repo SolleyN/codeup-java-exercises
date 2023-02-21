9908cd1e58b537b19b84e73a2500a489d9e109ea
Nickolas Solley <nicksolley@gmail.com>
2023-02-21 15:34:58 -0600

    Shape Exercise

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -2,10 +2,16 @@ package shapes;
 
 public class ShapesTest {
     public static void main(String[] args) {
+
         Measurable myShape = new Square(6.0);
 
+        System.out.println("The perimeter of the Square is: " + myShape.getPerimeter());
+        System.out.println("The Area of the Square is: " + myShape.getArea());
+
         Measurable myShape1 = new Rectangle(5, 5);
-        System.out.println(myShape.getArea());
+
+        System.out.println("The Area of the Rectangle is: " + myShape1.getArea());
+        System.out.println("The Perimeter of the Rectangle is: " + myShape1.getPerimeter());
 
     }
 
