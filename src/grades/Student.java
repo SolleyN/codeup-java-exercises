1b879b8219403177845e89d1a95d604a71462246
Nickolas Solley <nicksolley@gmail.com>
2023-02-22 12:31:30 -0600

    Student exercise

---
+++ src/grades/Student.java
@@ -0,0 +1,31 @@
+package grades;
+import java.util.ArrayList;
+
+public class Student {
+
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(String name) {
+        this.name = name;
+        this.grades = new ArrayList<Integer>();
+    }
+public String getName() {
+        return this.name;
+
+    }
+    public void addGrade(int grade) {
+this.grades.add(grade);
+    }
+
+public double getGradeAverage() {
+        if (this.grades.size() == 0) {
+            return 0;
+        }
+        int sum = 0;
+        for (int grade : this.grades) {
+            sum += grade;
+        }
+        return (double) sum / this.grades.size();
+}
+}
